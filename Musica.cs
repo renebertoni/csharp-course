49da842bbdaffec6e3c6db1c40760942d6d65ab7
renebertoni <renebertoni@gmail.com>
2024-02-27 19:24:21 -0300

    Create properties

--- Musica.cs
+++ Musica.cs
@@ -1,21 +1,44 @@
 class Music{
-    private string name;
-    private string artist;
-    private int duration;
+    private readonly string name;
+    private readonly string artist;
+    private readonly int duration;
     private bool available;
+    public bool canSwitchAvailable = true;
 
-    public Music(string name, string artist, int duration, bool available){
+    public bool Available
+    {
+        get
+        {
+            return available;
+        }
+        set
+        {
+            if(canSwitchAvailable)
+            {
+                available = value;
+            }
+            else
+            {
+                available = false;
+                Console.WriteLine("Você não pode alterar a propriedade Available!");
+            }
+        }
+    }
+
+
+    public Music(string name, string artist, int duration, bool available)
+    {
         this.name = name;
         this.artist = artist;
         this.duration = duration;
-        this.available = available;
+        Available = available;
     }
 
     public void ShowInformations()
     {
-        System.Console.WriteLine($"Name: {name}");
-        System.Console.WriteLine($"Artist: {artist}");
-        System.Console.WriteLine($"Duration: {duration}");
-        System.Console.WriteLine($"Available: {available}");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Artist: {artist}");
+        Console.WriteLine($"Duration: {duration}");
+        Console.WriteLine($"Available: {Available}");
     }
 }
--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fee18a4acaa837d38c5fbbae4928950a1429346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c44bb7c12837718e89a2bd3e8214291034c7f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
